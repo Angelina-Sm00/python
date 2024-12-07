575d23d4c31b5871991eadacad316ca55e71ca3c
Angelina-Sm00 <smilan0714@gmail.com>
2024-12-07 14:52:49 +0200

    hgh

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello World!')
+class Student:
+    def __init__(self,name):
+        self.name = name
+        self.progress = 0
+        self.energy = 50
+
+
+student = Student("Nikita")
+
+print (student.name)
+student1 = Student("Alex")
+
+student1.progress = 20
+
+print(student1.name, student1.progress)
+        
